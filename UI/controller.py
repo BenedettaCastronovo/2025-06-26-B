4f1da52de0c027a638c5802f520dc14d85d41c8f
benedetta <s327261@studenti.polito.it>
2026-06-24 15:11:49 +0200

    prima parte

--- UI/controller.py
+++ UI/controller.py
@@ -7,6 +7,10 @@ class Controller:
         self._view = view
         # the model, which implements the logic of the program and holds the data
         self._model = model
+        self._k = None
+        self._m = None
+        self.min = None
+        self.max = None
 
     def handleBuildGraph(self, e):
         if self._view._ddYear1.value > self._view._ddYear2.value or self._view._ddYear1.value is None or self._view._ddYear2.value is None:
@@ -33,7 +37,28 @@ class Controller:
 
 
     def handleCercaDreamChampionship(self, e):
-        pass
+        self._k = self._view._txtInSoglia.value
+        self._m = self._view._txtInNumDiEdizioni.value
+        if self._k == None or self._m == None:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("seleziona"))
+            self._view.update_page()
+            return
+        try:
+            self._k = int(self._k)
+            self._m = int(self._m)
+        except ValueError:
+            self._view._txt_result.controls.clear()
+            self._view._txt_result.controls.append(ft.Text("seleziona numeri valid"))
+            self._view.update_page()
+            return
+        self._view._txt_result.controls.clear()
+        path, bestI = self._model.cerca(self._k, self._m)
+        self._view._txt_result.controls.clear()
+        for n in path:
+            self._view._txt_result.controls.append(ft.Text(f"id: {n.circuitId} - nome: {n.name}"))
+        self._view._txt_result.controls.append(ft.Text(f"imprevisti: {bestI}"))
+        self._view.update_page()
 
     def fillDD(self):
         years = self._model.getAllYears()
--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import copy
+
 import networkx as nx
 
 from database.DAO import DAO
@@ -8,6 +10,10 @@ class Model:
         self._circ = DAO.getAllCircuits()
         self._grafo = nx.Graph()
         self.mappaC = {}
+        self._best = {}
+        self.somma = 0
+        self.listao = []
+        self.mas = []
 
     def getAllYears(self):
         return DAO.getAllYears()
@@ -31,8 +37,8 @@ class Model:
     def stampaDettagli(self):
             #listapesoM = list(sorted(self._grafo.edges(data=True), key=lambda x: x[2]["weight"], reverse=True))[:3]
             compC = list(nx.connected_components(self._grafo))  # RICORDA
-            mas = max(compC, key=len)
-            subgraph = self._grafo.subgraph(mas).copy()
+            self.mas = max(compC, key=len)
+            subgraph = self._grafo.subgraph(self.mas).copy()
             lista = []
             for n in subgraph.nodes():
                 edges = list(subgraph.edges(n, data=True))
@@ -40,8 +46,66 @@ class Model:
                     continue  # nodo isolato, salta
                 minino = min(edges, key=lambda x: x[2]['weight'])
                 lista.append((n, minino[2]["weight"]))
-            listao = sorted(lista, key=lambda x: x[1], reverse=True)
-            return mas, listao
+            self.listao = sorted(lista, key=lambda x: x[1], reverse=True)
+            return self.mas, self.listao
+
+
+    def cerca(self, k, m):
+        self._best = {}
+        self.somma = 0
+        #comp = self.listao meglio con mas perche senno lista di tupla piu difficult
+        comp = self.mas #cosi ho solo i nodi
+        for n in list(comp)[:5]:  # stampa i primi 5 nodi
+            print(f"{n.name} - edizioni: {len(n.piaz)}")
+        nodi_validi = [n for n in comp if self.is_valid(n, comp, m)]
+        print(f"nodi validi: {len(nodi_validi)}")
+        self.ric([], nodi_validi, k, m)
+        return self._best, self.somma
+
+    def ric(self, parziale, comp, k, m):
+        if len(parziale) == k:
+            print(f"trovato parziale pieno!")
+            if self.sommaimp(parziale) > self.somma:
+                self.somma = self.sommaimp(parziale)
+                self._best = copy.deepcopy(parziale)
+            return
+
+        #for n in self._grafo.neighbors(parziale[-1]):
+        for n in comp:
+            if n not in parziale and self.is_valid(n, comp, m):
+                parziale.append(n)
+                self.ric(parziale, comp, k, m)
+                parziale.pop()
+
+    def sommaimp(self, parziale):
+        i = 0
+        somma = 0
+        for c in parziale:
+            np = 0
+            npTot = 0
+            for t in c.piaz.values():
+                for p in t:  # p è la singola Posizione
+                    npTot += 1
+                    if p.time is not None:
+                        np += 1
+            if npTot > 0:
+                i = 1 - np/npTot
+            else:
+                i = 0
+            somma += i
+        return somma
+
+    def is_valid(self, n, comp, m):
+        if n in comp and len(n.piaz) >= m:
+            return True
+
+        return False
+
+
+
+
+
+
 
 
 
